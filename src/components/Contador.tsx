861274d8c40f147b453c7f11d0ae3bb1920edcb0
JOSE DE LOS SANTOS RAMOS <48066440+jose-web@users.noreply.github.com>
2024-09-21 20:05:38 +0000

    Componente Contador añadido

---
+++ src/components/Contador.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const Contador = () => {
+    const [tiempo, setTiempo] = useState(new Date(0, 0, 0, 0, 0, 5));
+
+    let interval: any;
+
+    const actualizarContador = () => {
+    
+        setTiempo(prevTiempo => {
+           
+            const nuevoTiempo = new Date(prevTiempo.getTime() - 1000);
+
+            if (nuevoTiempo.getMinutes() == 0 && nuevoTiempo.getSeconds() == 0) {
+                clearInterval(interval);
+            }
+
+            return nuevoTiempo;
+        });
+    };
+
+    useEffect(() => {
+        interval = setInterval(actualizarContador, 1000);
+        return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+    }, []);
+
+    // Renderizar el contador en formato MM:SS
+    return (
+        <div>
+            <h1>{tiempo.getMinutes().toString().padStart(2, '0')}:{tiempo.getSeconds().toString().padStart(2, '0')}</h1>
+        </div>
+    );
+};
+
