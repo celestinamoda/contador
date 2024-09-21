59a8392321ad7879ecb43ae2fdcb9fef57959375
JOSE DE LOS SANTOS RAMOS <48066440+jose-web@users.noreply.github.com>
2024-09-21 20:43:26 +0000

    Fuente de texto "Herr Von Muellerhoff" añadida

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				celestina: ['Herr Von Muellerhoff'],
+			  },
+		},
 	},
-	plugins: [],
-}
+	plugins: [
+		plugin(function ({ addBase }) {
+		  addBase({
+			'@font-face': {
+				fontFamily: 'Herr Von Muellerhoff',
+				fontWeight: '400',
+				src: 'url(/src/assets/fonts/HerrVonMuellerhoff-Regular.ttf) format("truetype")',
+			}
+		  })
+		}),
+	  ],
+	}
